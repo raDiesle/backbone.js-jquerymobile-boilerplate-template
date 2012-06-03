b4938a192d30bb2531823432a9f70f8a025627d6
David Amend <david.amend@it-amend.de>
2012-06-03 18:17:26 +0200

    initial backbone.js boilerplate template

---
+++ app/main.js
@@ -0,0 +1,57 @@
+require([
+  // Global
+  "app",
+
+  // Libs
+  "jquery",
+  "backbone"
+],
+
+function(app, $, Backbone, Example) {
+
+  // Defining the application router, you can attach sub routers here.
+  var Router = Backbone.Router.extend({
+    routes: {
+      "": "index"
+    },
+
+    index: function() {
+
+    }
+  });
+
+  // Treat the jQuery ready function as the entry point to the application.
+  // Inside this function, kick-off all initialization, everything up to this
+  // point should be definitions.
+  $(function() {
+    // Define your master router on the application namespace and trigger all
+    // navigation from this instance.
+    app.router = new Router();
+
+    // Trigger the initial route and enable HTML5 History API support
+    Backbone.history.start({ pushState: true });
+  });
+
+  // All navigation that is relative should be passed through the navigate
+  // method, to be processed by the router. If the link has a `data-bypass`
+  // attribute, bypass the delegation completely.
+  $(document).on("click", "a:not([data-bypass])", function(evt) {
+    // Get the anchor href and protcol
+    var href = $(this).attr("href");
+    var protocol = this.protocol + "//";
+
+    // Ensure the protocol is not part of URL, meaning it's relative.
+    if (href && href.slice(0, protocol.length) !== protocol &&
+        href.indexOf("javascript:") !== 0) {
+      // Stop the default event to ensure the link will not cause a page
+      // refresh.
+      evt.preventDefault();
+
+      // `Backbone.history.navigate` is sufficient for all Routers and will
+      // trigger the correct events. The Router's internal `navigate` method
+      // calls this anyways.
+      Backbone.history.navigate(href, true);
+    }
+  });
+
+});
