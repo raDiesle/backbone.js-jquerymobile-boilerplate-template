658db03985f6a840fbd918db4d0f27e3d07a07b2
David Amend <david.amend@it-amend.de>
2012-06-16 23:40:59 +0200

    merge

--- app/modules/view/abstract/BasicView.js
+++ app/modules/view/abstract/BasicView.js
@@ -12,6 +12,10 @@ function(app, _, Backbone, Handlebars) {
   var BasicView = app.module();
 
   BasicView = Backbone.View.extend({
+  	initialize : function() {
+  		Handlebars.registerHelper('whatis', function(param) {
+	    console.log(param);
+	});
 		 _.bindAll(); // this, 'render'
 		 this.render();
 	},
