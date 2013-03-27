b1ea55ccb7b83729357202cac7fa3cd0d0fb39a3
David Amend <david.amend@it-amend.de>
2013-03-27 11:56:45 +0100

    upgrade to grunt 0.4.0. BBB currently not supported, because it doesnt support newest version

--- app/jquerymobile.config.js
+++ app/jquerymobile.config.js
@@ -5,13 +5,17 @@ define(['jquery'], function ($) {
         $.mobile.pushStateEnabled = false;
         $.mobile.linkBindingEnabled = false; //-- works properly with jqm 1.1.1 rc1
 
+        // use ? $.mobile.autoInitializePage = false;
+
         $.mobile.defaultDialogTransition = "none";
         $.mobile.defaultPageTransition = "slidedown";
         $.mobile.page.prototype.options.degradeInputs.date = true;
         $.mobile.page.prototype.options.domCache = false;
 
         //enable flag to disable rendering
-        $.mobile.ignoreContentEnabled=true;
+        // $.mobile.ignoreContentEnabled=true;
+        $.mobile.pushStateEnabled = true;
+        $.mobile.phonegapNavigationEnabled = true;
         // enable loading page+icon
         $.mobile.loader.prototype.options.text = "loading";
         $.mobile.loader.prototype.options.textVisible = false;
